e179e105e8a4a77232e6f5af11128a11144df887
Askie Lin <askie_lin@trendmicro.com>
2020-05-27 15:27:25 +0800

    create card component with styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => {
   return (
     <div className='card-list'>
       {props.monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
   );
