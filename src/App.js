ee8b8b25c8190a01a416117c2c41b69e9d075f5c
Askie Lin <askie_lin@trendmicro.com>
2020-05-27 15:07:04 +0800

    create a first component with css-grid style and use it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -33,9 +34,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name='Askie'>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
