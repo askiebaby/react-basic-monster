53daae97bf6e3e6d891a57bc5a210d54c83b1761
Askie Lin <askie_lin@trendmicro.com>
2020-05-27 15:13:40 +0800

    move array into card-list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import './card-list.styles.css';
 
 export const CardList = (props) => {
-  return <div className='card-list'>{props.children}</div>;
+  return (
+    <div className='card-list'>
+      {props.monsters.map((monster) => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
+    </div>
+  );
 };
