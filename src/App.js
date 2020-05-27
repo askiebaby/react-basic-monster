12e71fda103c36a5408f12c34d0fc74dba557425
Askie Lin <askie_lin@trendmicro.com>
2020-05-27 14:03:04 +0800

    know lifecyle is come from React.Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       monsters: [
@@ -19,20 +18,27 @@ class App extends Component {
           name: 'Stone',
           id: 'd5cp',
         },
-      ]
-    }
+      ],
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ monsters: users });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={ monster.id }>{ monster.name }</h1>
-          ))
-        }
+      <div className='App'>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
-  } 
+  }
 }
 
 export default App;
