3fdf6ea4fd6e20a649607c10e2107a2718ee4440
Askie Lin <askie_lin@trendmicro.com>
2020-06-02 16:00:04 +0800

    Updates

---
+++ precache-manifest.03844b4553ca3368b40ac3159aec3e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64de5dd65eeefd82e493b34c975151a3",
+    "url": "/react-basic-monster/index.html"
+  },
+  {
+    "revision": "5a204e8038077f6a8fe3",
+    "url": "/react-basic-monster/static/css/main.7e6237c5.chunk.css"
+  },
+  {
+    "revision": "ecde49e1ef6ac8acbde8",
+    "url": "/react-basic-monster/static/js/2.e7c28c8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-monster/static/js/2.e7c28c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a204e8038077f6a8fe3",
+    "url": "/react-basic-monster/static/js/main.4340828d.chunk.js"
+  },
+  {
+    "revision": "9944bc17b4a546383dd6",
+    "url": "/react-basic-monster/static/js/runtime-main.7cb99056.js"
+  }
+]);
