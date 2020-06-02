f42334a0fe98c0ceef159604fca14c650020959e
Askie Lin <askie_lin@trendmicro.com>
2020-06-02 15:53:40 +0800

    add homepage styles and import google font

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monster Bolodex</h1>
         <SearchBox
           placeholder='search monster'
           handleChange={this.handleChange}
